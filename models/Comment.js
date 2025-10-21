b8df9c3f504d4972069ca62cf6c285035e221704
Fatima-Zaid <fatimaameer957@gmail.com>
2025-10-21 11:31:00 +0300

    Update comment schema

--- models/Comment.js
+++ models/Comment.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose")
 const { Schema } = require("mongoose")
 
 const commentSchema = new Schema({
-  // username: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "Post",
-  // },
-  // postId: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "Post",
-  // },
+  username: {
+    type: mongoose.Schema.Types.String,
+    ref: "Post",
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
   description: {
     type: String,
     required: true,
